a7a0b2974c0375258e21a49b5418c8ff49f1ae83
MercyFlesh <fleshmercy@gmail.com>
2021-03-11 21:57:47 +0300

    add implementation Rational class

--- lab_2/rational.hpp
+++ lab_2/rational.hpp
@@ -4,12 +4,15 @@
 
 class Rational
 {
+	static int gcd(int a, int b);
+	void reduct();
+
 public:
 	Rational() = default;
 	Rational(int a);
 	Rational(int a, int b);
-
-	void reduct();
+	Rational(double a);
+	Rational(const Rational& rt);
 	
 	Rational operator- () const;
 	Rational operator+ (const Rational& rhs) const;
@@ -19,11 +22,12 @@ public:
 	
 	Rational& operator*= (const Rational& rhs);
 	Rational& operator/= (const Rational& rhs);
-	
-	bool operator== (const Rational& rhs) const;
-	bool operator!= (const Rational& rhs) const;
-	bool operator< (const Rational& rhs) const;
-	bool operator>= (const Rational& rhs) const;
+
+	operator double() const;
+	//bool operator== (const Rational& rhs) const;
+	//bool operator!= (const Rational& rhs) const;
+	//bool operator< (const Rational& rhs) const;
+	//bool operator>= (const Rational& rhs) const;
 	
 	friend Rational sqrt(const Rational& rt);
 	friend Rational pow(const Rational& rt, const int& power);
