1e36fd9bcbe313378ca6da38e2f96c03723fd3e8
MercyFlesh <fleshmercy@gmail.com>
2021-04-19 16:43:08 +0300

    update control tab

--- course_work/client/controlwindow.cpp
+++ course_work/client/controlwindow.cpp
@@ -1,6 +1,7 @@
 #include "controlwindow.hpp"
 
-#include <QSizePolicy>
+#include <QJsonObject>
+#include <QJsonDocument>
 
 ControlWindow::ControlWindow(QWidget* parent)
     : QWidget(parent),
@@ -8,12 +9,15 @@ ControlWindow::ControlWindow(QWidget* parent)
       vbox_ptr(new QVBoxLayout),
       grid_form_ptr(new QGridLayout(this))
 {
-    //vbox_ptr->setSpacing(100);
-
     grid_form_ptr->setVerticalSpacing(15);
     grid_form_ptr->setHorizontalSpacing(10);
+
     title_label_ptr = std::make_unique<QLabel>("Request", this);
     title_label_ptr->setStyleSheet("QLabel {font: 16pt;}");
+
+    request_number_ptr = std::make_unique<QLineEdit>("#", this);
+    request_number_ptr->setReadOnly(true);
+
     name_label_ptr = std::make_unique<QLabel>("Name", this);
     email_label_ptr = std::make_unique<QLabel>("Email", this);
     descript_label_ptr = std::make_unique<QLabel>("Description", this);
@@ -21,22 +25,92 @@ ControlWindow::ControlWindow(QWidget* parent)
     email_edit_ptr = std::make_unique<QLineEdit>(this);
     descript_edit_ptr = std::make_unique<QTextEdit>(this);
 
+    send_btn_ptr = std::make_unique<QPushButton>("send", this);
+
     name_label_ptr->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
     email_label_ptr->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
     descript_label_ptr->setAlignment(Qt::AlignRight | Qt::AlignTop);
 
     grid_form_ptr->addWidget(title_label_ptr.get(), 0, 0, 1, 1);
-    grid_form_ptr->addWidget(name_label_ptr.get(), 1, 0, 1, 1);
-    grid_form_ptr->addWidget(name_edit_ptr.get(), 1, 1, 1, 1);
-    grid_form_ptr->addWidget(email_label_ptr.get(), 2, 0, 1, 1);
-    grid_form_ptr->addWidget(email_edit_ptr.get(), 2, 1, 1, 1);
-    grid_form_ptr->addWidget(descript_label_ptr.get(), 3, 0, 1, 1);
-    grid_form_ptr->addWidget(descript_edit_ptr.get(), 3, 1, 4, 1);
-
-
+    grid_form_ptr->addWidget(request_number_ptr.get(), 1, 1, 1, 2);
+    grid_form_ptr->addWidget(name_label_ptr.get(), 2, 0, 1, 1);
+    grid_form_ptr->addWidget(name_edit_ptr.get(), 2, 1, 1, 2);
+    grid_form_ptr->addWidget(email_label_ptr.get(), 3, 0, 1, 1);
+    grid_form_ptr->addWidget(email_edit_ptr.get(), 3, 1, 1, 2);
+    grid_form_ptr->addWidget(descript_label_ptr.get(), 4, 0, 1, 1);
+    grid_form_ptr->addWidget(descript_edit_ptr.get(), 4, 1, 1, 2);
+    grid_form_ptr->addWidget(send_btn_ptr.get(), 5, 2, 1, 1);
 
     vbox_ptr->addLayout(grid_form_ptr.get());
-
-
     setLayout(vbox_ptr.get());
+
+    connect(send_btn_ptr.get(), SIGNAL(pressed()), this, SLOT(validate_form()));
+    connect(this, SIGNAL(isValid()), this, SLOT(send_request()));
+    connect(this, SIGNAL(control_response(QJsonObject)), this, SLOT(print_request_number(QJsonObject)));
+}
+
+
+void ControlWindow::validate_form()
+{
+    if (!name_edit_ptr->text().isEmpty() &&
+        !email_edit_ptr->text().isEmpty() &&
+        !descript_edit_ptr->toPlainText().isEmpty())
+    {
+        name_edit_ptr->setStyleSheet("");
+        email_edit_ptr->setStyleSheet("");
+        descript_edit_ptr->setStyleSheet("");
+
+        emit isValid();
+        return;
+    }
+
+    if (name_edit_ptr->text().isEmpty())
+    {
+        name_edit_ptr->setStyleSheet("QLineEdit {border: 1px solid #B9103C}");
+    }
+    else
+    {
+        name_edit_ptr->setStyleSheet("");
+    }
+
+    if (email_edit_ptr->text().isEmpty())
+    {
+        email_edit_ptr->setStyleSheet("QLineEdit {border: 1px solid #B9103C}");
+    }
+    else
+    {
+        email_edit_ptr->setStyleSheet("");
+    }
+
+    if (descript_edit_ptr->toPlainText().isEmpty())
+    {
+        descript_edit_ptr->setStyleSheet("QTextEdit {border: 1px solid #B9103C}");
+    }
+    else
+    {
+        descript_edit_ptr->setStyleSheet("");
+    }
+}
+
+
+void ControlWindow::send_request()
+{
+    QJsonObject json_request;
+
+    json_request.insert("name", name_edit_ptr->text());
+    json_request.insert("email", email_edit_ptr->text());
+    json_request.insert("description", descript_edit_ptr->toPlainText());
+
+    emit send_control_req(json_request);
+}
+
+
+void ControlWindow::print_request_number(QJsonObject response)
+{
+    request_number_ptr->setText("#" + response.value("number").toString());
+    request_number_ptr->setStyleSheet("QLineEdit {border: 1px solid #1AD39A}");
+
+    name_edit_ptr->clear();
+    email_edit_ptr->clear();
+    descript_edit_ptr->clear();
 }
--- course_work/client/controlwindow.hpp
+++ course_work/client/controlwindow.hpp
@@ -9,6 +9,7 @@
 #include <QLabel>
 #include <QLineEdit>
 #include <QTextEdit>
+#include <QJsonObject>
 
 #include <memory>
 
@@ -18,13 +19,27 @@ class ControlWindow : public QWidget
 
     std::unique_ptr<QLabel> title_label_ptr, name_label_ptr;
     std::unique_ptr<QLabel> email_label_ptr, descript_label_ptr;
+    std::unique_ptr<QLineEdit> request_number_ptr;
     std::unique_ptr<QLineEdit> name_edit_ptr, email_edit_ptr;
     std::unique_ptr<QTextEdit> descript_edit_ptr;
+
+    std::unique_ptr<QPushButton> send_btn_ptr;
 
     std::unique_ptr<QGridLayout> grid_form_ptr;
     std::unique_ptr<QHBoxLayout> hbox_ptr;
     std::unique_ptr<QVBoxLayout> vbox_ptr;
+
 public:
     explicit ControlWindow(QWidget* parent = nullptr);
     ~ControlWindow() = default;
+
+private slots:
+    void validate_form();
+    void send_request();
+    void print_request_number(QJsonObject response);
+
+signals:
+    void isValid();
+    void send_control_req(QJsonObject request);
+    void control_response(QJsonObject response);
 };
