5ab8e5173e6eb09b55bbc77a5fc9cbb0faf1041d
MercyFlesh <con2288@mail.ru>
2021-04-19 21:39:00 +0000

    update state tab interface

--- course_work/client/statewindow.cpp
+++ course_work/client/statewindow.cpp
@@ -1,8 +1,9 @@
 #include "statewindow.hpp"
 
+
 StateWindow::StateWindow(QWidget* parent)
     : QWidget(parent),
-      vbox_main_layout(new QVBoxLayout(this))
+      grid_layout(new QGridLayout(this))
 {
     free_managers_label = std::make_unique<QLabel>("Free managers", this);
     busy_managers_label = std::make_unique<QLabel>("Busy managers", this);
@@ -16,30 +17,18 @@ StateWindow::StateWindow(QWidget* parent)
     update_busy_btn = std::make_unique<QPushButton>("update", this);
     update_queue_btn = std::make_unique<QPushButton>("update", this);
 
-    vbox_free = std::make_unique<QVBoxLayout>(this);
-    vbox_busy = std::make_unique<QVBoxLayout>(this);
-    hbox_managers_lists = std::make_unique<QHBoxLayout>(this);
-    vbox_free->setSpacing(15);
-    vbox_busy->setSpacing(15);
-    hbox_managers_lists->setSpacing(15);
+    grid_layout->setVerticalSpacing(10);
+    grid_layout->setHorizontalSpacing(15);
 
-    vbox_free->addWidget(free_managers_label.get());
-    vbox_free->addWidget(free_managers_list.get());
-    vbox_free->addWidget(update_free_btn.get());
-    vbox_busy->addWidget(busy_managers_label.get());
-    vbox_busy->addWidget(busy_managers_list.get());
-    vbox_busy->addWidget(update_busy_btn.get());
+    grid_layout->addWidget(free_managers_label.get(), 0, 0, 1, 1);
+    grid_layout->addWidget(free_managers_list.get(), 1, 0, 1, 1);
+    grid_layout->addWidget(update_free_btn.get(), 2, 0, 1, 1);
+    grid_layout->addWidget(busy_managers_label.get(), 0, 2, 1, 1);
+    grid_layout->addWidget(busy_managers_list.get(), 1, 2, 1, 1);
+    grid_layout->addWidget(update_busy_btn.get(), 2, 2, 1, 1);
+    grid_layout->addWidget(queue_list_label.get(), 3, 0, 1, 1);
+    grid_layout->addWidget(queue_requests_list.get(), 4, 0, 1, 1);
+    grid_layout->addWidget(update_queue_btn.get(), 5, 0, 1, 1);
 
-    hbox_managers_lists->addLayout(vbox_free.get());
-    hbox_managers_lists->addLayout(vbox_busy.get());
-
-    vbox_queue_list->addWidget(queue_list_label.get());
-    vbox_queue_list->addWidget(queue_requests_list.get());
-    vbox_queue_list->addWidget(update_queue_btn.get());
-
-    vbox_main_layout->setSpacing(15);
-    vbox_main_layout->addLayout(hbox_managers_lists.get());
-    vbox_main_layout->addLayout(vbox_queue_list.get());
-
-    setLayout(vbox_main_layout.get());
+    setLayout(grid_layout.get());
 }
--- course_work/client/statewindow.hpp
+++ course_work/client/statewindow.hpp
@@ -1,15 +1,12 @@
 #pragma once
-
 #include <QObject>
 #include <QWidget>
 #include <QLabel>
 #include <QListWidget>
 #include <QPushButton>
-#include <QVBoxLayout>
-#include <QHBoxLayout>
+#include <QGridLayout>
 
 #include <memory>
-
 
 class StateWindow : public QWidget
 {
@@ -20,11 +17,8 @@ class StateWindow : public QWidget
     std::unique_ptr<QListWidget> queue_requests_list;
     std::unique_ptr<QPushButton> update_free_btn, update_busy_btn, update_queue_btn;
 
-    std::unique_ptr<QVBoxLayout> vbox_free, vbox_busy, vbox_queue_list;
-    std::unique_ptr<QHBoxLayout> hbox_managers_lists;
-    std::unique_ptr<QVBoxLayout> vbox_main_layout;
+    std::unique_ptr<QGridLayout> grid_layout;
 
 public:
     explicit StateWindow(QWidget* parent = nullptr);
-    ~StateWindow() = default;
 };
